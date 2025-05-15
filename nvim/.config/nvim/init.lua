5aa352f4ee86edf462ca77698e895d030e9d619b
Fabio Oliveira <fabio.oliveira1403@outlook.com>
2025-05-14 22:42:30 -0300

    chore: Modulariozando stash

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,23 +1,9 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=3")
-
--- Neovim
+-- Leader Configuration
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
-vim.keymap.set('n', '<leader>q', ':q<CR>', {silent = true})
-
 -- Lazy configuration
 require("config.lazy")
+-- Lua Configuration setup
+require("vim-options")
 
--- Telescop configuration
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-
--- Neotree
-vim.keymap.set('n', '<leader>n', ':Neotree filesystem reveal left<CR>', { silent = true })
 
